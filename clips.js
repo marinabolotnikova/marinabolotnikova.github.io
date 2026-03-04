7e717984d79278bfe55e3a251c3ae80d9b6e1ff5
benn-mctosh <107890667+benn-mctosh@users.noreply.github.com>
2026-03-04 11:46:16 -0600

    Update clips.js

--- clips.js
+++ clips.js
@@ -38,6 +38,14 @@ const tags = [
 
 var clips = [
     {
+        "hed" : "The biggest drawback of driverless cars",
+        "dek" : "Driverless cars could save thousands of lives. They might also break our cities.",
+        "outlet" : "Vox",
+        "date" : "2026-03-04",
+        "link" : "https://www.vox.com/future-perfect/481371/driverless-cars-avs-safety-miles-driven",
+        "tags" : "science, urbanism"
+    },
+    {
         "hed" : "One good thing the Trump administration might actually do for science",
         "dek" : "A primate research center could become a sanctuary as the NIH pushes to move biomedical research beyond animal testing.",
         "outlet" : "Vox",
